d34947acb8b35a5f977dff96d11a74955d529891
Krittapas Rungsimontuchat <nineten609@gmail.com>
2024-05-22 18:23:38 +0700

    feat: form

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -8,19 +8,28 @@ import { Button } from '@/components/ui/button';
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectContent,
+  SelectValue,
+} from '@/components/ui/select';
 
 const formSchema = z.object({
   name: z.string().min(2, {
     message: 'Username must be at least 2 characters.',
   }),
   email: z.string().email(),
+  Month: z.string().min(1),
+  Day: z.string().min(1),
+  Year: z.string().min(1),
   password: z
     .string()
     .min(8, { message: 'Password must be at least 8 characters long' })
@@ -34,12 +43,34 @@ const formSchema = z.object({
 });
 
 export default function SignUpPage() {
+  const months = [
+    { name: 'January', value: '01', days: 31 },
+    { name: 'February', value: '02', days: 28 },
+    { name: 'March', value: '03', days: 31 },
+    { name: 'April', value: '04', days: 30 },
+    { name: 'May', value: '05', days: 31 },
+    { name: 'June', value: '06', days: 30 },
+    { name: 'July', value: '07', days: 31 },
+    { name: 'August', value: '08', days: 31 },
+    { name: 'September', value: '09', days: 30 },
+    { name: 'October', value: '10', days: 31 },
+    { name: 'November', value: '11', days: 30 },
+    { name: 'December', value: '12', days: 31 },
+  ];
+
+  const years = Array.from({ length: 2024 - 1900 + 1 }, (_, i) =>
+    (1900 + i).toString(),
+  ).sort((a, b) => parseInt(b) - parseInt(a));
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: '',
       email: '',
       password: '',
+      Month: '',
+      Day: '',
+      Year: '',
     },
   });
 
@@ -48,56 +79,153 @@ export default function SignUpPage() {
   }
   return (
     <div className="flex flex-col min-h-screen justify-center items-center p-4 sm:p-6 lg:p-8">
-      <div className=' w-full max-w-md'>
-      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-center mb-7">
-        Create an account
-      </h4>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 ">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input type="password" placeholder="password" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className="w-full">
-            Submit
-          </Button>
-        </form>
-      </Form>
+      <div className=" w-full max-w-md">
+        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-center mb-7">
+          Create an account
+        </h4>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 ">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <p className="leading-7 [&:not(:first-child)]:mt-6">
+              Date of Birth
+            </p>
+            <div className=" flex flex-row justify-between gap-3">
+              <div className=" flex-1">
+                <FormField
+                  control={form.control}
+                  name="Month"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                          value={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Month" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="Doctor">Doctor</SelectItem>
+                            <SelectItem value="Mr">Mr</SelectItem>
+                            <SelectItem value="Mrs">Mrs</SelectItem>
+                            <SelectItem value="Miss">Miss</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+              </div>
+              <div className=" flex-1">
+                <FormField
+                  control={form.control}
+                  name="Day"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                          value={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Day" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="Doctor">Doctor</SelectItem>
+                            <SelectItem value="Mr">Mr</SelectItem>
+                            <SelectItem value="Mrs">Mrs</SelectItem>
+                            <SelectItem value="Miss">Miss</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+              </div>
+              <div className=" flex-1">
+                <FormField
+                  control={form.control}
+                  name="Year"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                          value={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Year" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            {years.map((year) => (
+                              <SelectItem key={year} value={year}>
+                                {year}
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+              </div>
+            </div>
+
+            <Button type="submit" className="w-full">
+              Submit
+            </Button>
+          </form>
+        </Form>
       </div>
     </div>
   );
