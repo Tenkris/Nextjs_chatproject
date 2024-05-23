2096e8804122871f52451e3fcfa5a7d1354bd9ae
Krittapas Rungsimontuchat <nineten609@gmail.com>
2024-05-23 10:28:15 +0700

    feat: google login

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -14,6 +14,9 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import Link from 'next/link';
+import { GoogleIcon, GitHubIcon } from '@/components/ui/Icons';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 const formSchema = z.object({
   email: z.string().email(),
   password: z
@@ -29,6 +32,7 @@ const formSchema = z.object({
 });
 
 export default function SignInPage() {
+  const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -36,6 +40,18 @@ export default function SignInPage() {
       password: '',
     },
   });
+  const handleGoogleSignIn = async (e: any) => {
+    e.preventDefault();
+    try {
+      const result: any = await signIn('google', { callbackUrl: '/profile' });
+
+      if (result.error) {
+        console.error(result.error);
+      } else router.push('/profile');
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values);
@@ -95,6 +111,28 @@ export default function SignInPage() {
               <Button type="submit" className=" w-full">
                 Log in
               </Button>
+              <p className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 font-medium text-center">
+                or continue with
+              </p>
+              <div className=" flex flex-row justify-center gap-3">
+                <Button size="lg" className=" rounded-full p-2" onClick={ handleGoogleSignIn}>
+                  <GoogleIcon />
+                </Button>
+                <Button size="lg" className=" rounded-full p-2">
+                  <GitHubIcon />
+                </Button>
+              </div>
+              <div className=" pt-10 ">
+                <p className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 font-medium text-center">
+                  Need an account?{' '}
+                  <Link
+                    href={{ pathname: '/sign-up' }}
+                    className=" font-extrabold"
+                  >
+                    Sign up
+                  </Link>
+                </p>
+              </div>
             </div>
           </form>
         </Form>
